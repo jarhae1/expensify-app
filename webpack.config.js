56a2d687dc0b266a2b5bb11076d91cf8607584bd
jarhae <JaredCannon@Jareds-MacBook-Pro.local>
2019-06-10 22:16:59 -0600

    trying to fix heroku error

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,48 @@
-// entry -> ouput
-
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = (env)=>{
-    const isProduction = env === 'production';
-    const CSSExtract = new ExtractTextPlugin('styles.css')
-    return{
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
+
+  return {
     entry: './src/app.js',
     output: {
-        path:path.join(__dirname,'public'),
-        filename:'bundle.js'
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
     },
-    module:{
-        rules:[{
-            loader:'babel-loader',
-            test:/.\js$/,
-            exclude:/node_modules/
-        },{
-            test:/\.s?css$/,
-            use:  CSSExtract.extract({
-                use:[
-                    {loader:'css-loader',
-                options:{
-                    sourceMap:true
-                }
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
             },
-               {loader:'sass-loader',
-               options:{
-                   sourceMap:true
-               }
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
             }
-                ]
-            })
-        }]
+          ]
+        })
+      }]
     },
-    plugins:[
-        CSSExtract
+    plugins: [
+      CSSExtract
     ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
-    devServer:{
-        contentBase: path.join(__dirname,'public'),
-        historyApiFallback:true
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
     }
-    }
-}
-
+  };
+};
